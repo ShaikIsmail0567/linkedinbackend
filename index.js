8de08aad9ca7c0c8148f71a8c4e61519a065d7cb
ShaikIsmail0567 <sk.mdismail056@gmail.com>
2023-02-20 17:33:53 +0530

    BAckend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
 app.use(express.json())
+app.use(cors())
 const { connection } = require("./config/db")
 const { postrouter } = require("./controllers/post.router")
 const {userrouter}=require("./controllers/user.route")
